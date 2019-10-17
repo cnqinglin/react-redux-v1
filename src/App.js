59c4245cc314ebc80ec9c8f8a86c34c49768dc06
cnqinglin <2207328000@qq.com>
2019-10-17 23:23:57 +0800

    v2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
-  add1(){
-    this.props.onAdd1()
-  }
-  add2(){
-    this.props.onAdd2()
-  }
-  add3(){
-    this.props.onAdd3()
-  }
-  add4(){
-    this.props.onAdd4()
-  }
   render() {
     return ( 
       <div className = "App">
         <div>
-          你点击了 <span id = "value" > {this.props.value} </span> 
+          你点击了 <span id = "value" > {this.props.n} </span> 
           <div>
-            <button id = "add1" onClick={()=>this.add1()}> +1 </button> 
-            <button id = "minus1" onClick={()=>this.add2()}> +2 </button> 
-            <button id = 'add1IfOdd' onClick={()=>this.add3()}> 如果是单数就 + 1 </button> 
-            <button id = "add1After2Sec" onClick={()=>this.add4()}> 两秒钟后 + 1 </button> 
+            <button id = "add1" onClick={()=>this.props.add1()}> +1 </button> 
+            <button id = "minus1"> +2 </button> 
+            <button id = 'add1IfOdd'> 如果是单数就 + 1 </button> 
+            <button id = "add1After2Sec"> 两秒钟后 + 1 </button> 
           </div> 
         </div>
       </div>
@@ -30,4 +19,19 @@ class App extends React.Component {
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  return {
+    n: state.n
+  }
+}
+function  mapDispatchToProps(dispatch){
+    return {
+      add1:()=>{
+        dispatch({type:'add',payload:1})
+      }
+    }
+}
+  
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
